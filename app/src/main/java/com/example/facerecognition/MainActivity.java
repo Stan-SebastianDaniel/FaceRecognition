267ea95e0c6253de04e768c62015a86b5fa1db9f
Mihai Doinea <mihai.doinea@oracle.com>
2023-01-15 13:00:07 +0200

    Face detection

--- app/src/main/java/com/example/facerecognition/MainActivity.java
+++ app/src/main/java/com/example/facerecognition/MainActivity.java
@@ -2,13 +2,116 @@ package com.example.facerecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    JavaCameraView javaCameraView;
+    private CascadeClassifier faceDetector;
+
+    private LoaderCallbackInterface initCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    InputStream inputStream = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                    File cascadeClassifier = getDir("cascade", Context.MODE_PRIVATE);
+                    File lbpClassifier = new File(cascadeClassifier, "lbpcascade_frontalface.xml");
+                    FileOutputStream fos = null;
+
+                    try {
+                        fos = new FileOutputStream(lbpClassifier);
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            fos.write(buffer, 0, bytesRead);
+                        }
+                        inputStream.close();
+                        fos.close();
+
+                        faceDetector = new CascadeClassifier(lbpClassifier.getAbsolutePath());
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:
+                    super.onManagerConnected(status);
+            }
+        }
+    };
+    private Mat matRGB;
+    private Mat matGrey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        javaCameraView = findViewById(R.id.javaCameraView);
+
+        if(!OpenCVLoader.initDebug())
+        {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, initCallback);
+        }
+        else
+            initCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        javaCameraView.setCvCameraViewListener(this);
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        matRGB = new Mat();
+        matGrey = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        matRGB.release();
+        matGrey.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        matRGB = inputFrame.rgba();
+        matGrey = inputFrame.gray();
+        MatOfRect faces = new MatOfRect();
+        faceDetector.detectMultiScale(matRGB, faces);
+        for(Rect rect: faces.toArray())
+        {
+            Imgproc.rectangle(matRGB, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(255, 0, 0));
+        }
+        return matRGB;
     }
 }
